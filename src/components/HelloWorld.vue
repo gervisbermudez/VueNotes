bb1cb85d16493f3e0a09cfa9f6cc238aa9049daf
Gervis Bermudez <gervisbermudez@outlook.com>
2021-12-29 11:54:16 -0300

    master changes

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Home</h1>
+    <router-link to="/Login">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,4 @@
 <template>
-  <h1>Login</h1>
-  <router-link to="/">Home</router-link>
 </template>
 
 <script>
